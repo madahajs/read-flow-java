cfb7ff3b481df0f3d8b07068c75153ed0b251e13
masixin <masixin@dianju>
2025-12-29 20:40:00 +0800

    initial commit

---
+++ src/main/java/com/qizhi/readflow/ReadflowApplication.java
@@ -0,0 +1,13 @@
+package com.qizhi.readflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qizhi/readflow/ReadflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qizhi.readflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
